4fc20769f29f53b3521edcabf66db88816e4465f
liakosV <billiak@hotmail.com>
2026-04-28 11:39:50 +0300

    feat: User entity created

---
+++ src/main/java/com/project/fitness_nutrition_plan/model/User.java
@@ -0,0 +1,38 @@
+package com.project.fitness_nutrition_plan.model;
+
+import com.project.fitness_nutrition_plan.model.static_data.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+}
