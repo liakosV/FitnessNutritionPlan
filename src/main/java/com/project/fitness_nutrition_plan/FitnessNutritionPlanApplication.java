426a9068b14152f56692b78da8ced9dec8cf2ade
liakosV <billiak@hotmail.com>
2026-04-28 11:09:48 +0300

    Initial commit

---
+++ src/main/java/com/project/fitness_nutrition_plan/FitnessNutritionPlanApplication.java
@@ -0,0 +1,13 @@
+package com.project.fitness_nutrition_plan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessNutritionPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessNutritionPlanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/fitness_nutrition_plan/FitnessNutritionPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.fitness_nutrition_plan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessNutritionPlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
