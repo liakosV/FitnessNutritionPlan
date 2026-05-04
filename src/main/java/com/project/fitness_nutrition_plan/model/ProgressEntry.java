3478b5a2f36a37abd102b57ac5a669f194e43a5b
liakosV <billiak@hotmail.com>
2026-05-04 19:25:55 +0300

    feat: add ProgressEntry entity with JPA annotations to track user progress

---
+++ src/main/java/com/project/fitness_nutrition_plan/model/ProgressEntry.java
@@ -0,0 +1,37 @@
+package com.project.fitness_nutrition_plan.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "progress_entries")
+public class ProgressEntry {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private double weight;
+
+    @Column(name = "body_fat", nullable = false)
+    private double bodyFat;
+
+    private String notes;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
