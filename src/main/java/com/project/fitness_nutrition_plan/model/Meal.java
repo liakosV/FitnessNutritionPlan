07a7eec1df621e38c285ed88f9366c8740222c3f
liakosV <billiak@hotmail.com>
2026-05-04 19:24:23 +0300

    feat: add Meal entity with JPA annotations

---
+++ src/main/java/com/project/fitness_nutrition_plan/model/Meal.java
@@ -0,0 +1,39 @@
+package com.project.fitness_nutrition_plan.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "meals")
+public class Meal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer calories;
+
+    @Column(nullable = false)
+    private Double protein;
+
+    @Column(nullable = false)
+    private Double carbs;
+
+    @Column(nullable = false)
+    private Double fat;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "nutrition_plan_id", nullable = false)
+    private NutritionPlan nutritionPlan;
+}
