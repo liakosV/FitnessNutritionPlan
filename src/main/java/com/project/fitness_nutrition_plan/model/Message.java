6d6971555542846128a512d15956759e7ad56fff
liakosV <billiak@hotmail.com>
2026-05-04 19:24:45 +0300

    feat: add Message entity with JPA annotations to support message data modeling

---
+++ src/main/java/com/project/fitness_nutrition_plan/model/Message.java
@@ -0,0 +1,36 @@
+package com.project.fitness_nutrition_plan.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "messages")
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(name = "timestamp", nullable = false)
+    private LocalDateTime timeStamp;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id", nullable = false)
+    private User sender;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiver_id", nullable = false)
+    private User receiver;
+}
