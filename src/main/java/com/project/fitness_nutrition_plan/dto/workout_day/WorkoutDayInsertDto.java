a7903506f992b7bd4b3b3acf63ca6c51c78d8fd7
liakosV <billiak@hotmail.com>
2026-05-05 13:53:47 +0300

    feat: add WorkoutDayInsertDto to handle workout day creation requests

---
+++ src/main/java/com/project/fitness_nutrition_plan/dto/workout_day/WorkoutDayInsertDto.java
@@ -0,0 +1,19 @@
+package com.project.fitness_nutrition_plan.dto.workout_day;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class WorkoutDayInsertDto {
+
+    @NotBlank(message = "Day name cannot be empty")
+    private String dayName;
+
+    @NotNull(message = "Workout program id cannot be empty")
+    private Long workoutProgramId;
+}
