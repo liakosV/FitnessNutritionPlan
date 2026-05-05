a20970f8ccafab0d9c883e56a5599c1880a3edba
liakosV <billiak@hotmail.com>
2026-05-05 13:53:08 +0300

    feat: add ProgramDayUpdateDto to handle program day update requests

---
+++ src/main/java/com/project/fitness_nutrition_plan/dto/workout_day/ProgramDayUpdateDto.java
@@ -0,0 +1,12 @@
+package com.project.fitness_nutrition_plan.dto.workout_day;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProgramDayUpdateDto {
+    private String dayName;
+}
