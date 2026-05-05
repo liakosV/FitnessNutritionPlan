31b93a8ee2b17bf641c76a6b7e5ba029521f878a
liakosV <billiak@hotmail.com>
2026-05-05 13:54:08 +0300

    feat: add WorkoutProgramReadDto to handle workout program data retrieval

---
+++ src/main/java/com/project/fitness_nutrition_plan/dto/wokrout_program/WorkoutProgramReadDto.java
@@ -0,0 +1,26 @@
+package com.project.fitness_nutrition_plan.dto.wokrout_program;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class WorkoutProgramReadDto {
+
+    private Long id;
+    private String uuid;
+
+    private String name;
+    private String description;
+    private boolean active;
+
+    private Long coachId;
+    private String coachUsername;
+
+    private List<Long> assignedUserIds;
+    private List<String> assignedUsernames;
+}
