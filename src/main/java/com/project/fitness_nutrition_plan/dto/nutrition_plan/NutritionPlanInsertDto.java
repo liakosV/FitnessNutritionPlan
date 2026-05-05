cba39b1171acabcbbe9385e523b3fb4d41a9b924
liakosV <billiak@hotmail.com>
2026-05-05 13:52:50 +0300

    feat: add NutritionPlanInsertDto to handle nutrition plan creation requests

---
+++ src/main/java/com/project/fitness_nutrition_plan/dto/nutrition_plan/NutritionPlanInsertDto.java
@@ -0,0 +1,25 @@
+package com.project.fitness_nutrition_plan.dto.nutrition_plan;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NutritionPlanInsertDto {
+
+    @NotBlank(message = "Title cannot be empty")
+    private String title;
+
+    @NotBlank(message = "Description cannot be empty")
+    private String description;
+
+    private boolean active;
+
+
+    @NotNull(message = "Assigned user id cannot be empty")
+    private Long assignedUserId;
+}
