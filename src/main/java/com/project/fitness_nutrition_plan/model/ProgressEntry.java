595582f8447c874b686e1929b6ff0d4acf5e8991
liakosV <billiak@hotmail.com>
2026-05-05 13:53:15 +0300

    feat: make user association in ProgressEntry mandatory

--- src/main/java/com/project/fitness_nutrition_plan/model/ProgressEntry.java
+++ src/main/java/com/project/fitness_nutrition_plan/model/ProgressEntry.java
@@ -32,6 +32,6 @@ public class ProgressEntry {
     private LocalDate date;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 }
