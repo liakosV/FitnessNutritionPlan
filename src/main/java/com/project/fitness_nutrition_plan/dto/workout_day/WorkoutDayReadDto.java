99b561fa7ee9dd8145f201003635c41b396d3f8b
liakosV <billiak@hotmail.com>
2026-05-05 13:53:53 +0300

    feat: add WorkoutDayReadDto to handle workout day data retrieval

---
+++ src/main/java/com/project/fitness_nutrition_plan/dto/workout_day/WorkoutDayReadDto.java
@@ -0,0 +1,16 @@
+package com.project.fitness_nutrition_plan.dto.workout_day;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class WorkoutDayReadDto {
+    private Long id;
+    private String name;
+
+    private Long workoutProgramId;
+    private String workoutProgramName;
+}
