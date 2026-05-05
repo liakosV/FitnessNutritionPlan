a126e886507c066169c1e54af9c1cc24cae4ffdf
liakosV <billiak@hotmail.com>
2026-05-05 13:52:33 +0300

    feat: add MealUpdateDto to handle meal update requests

---
+++ src/main/java/com/project/fitness_nutrition_plan/dto/meal/MealUpdateDto.java
@@ -0,0 +1,18 @@
+package com.project.fitness_nutrition_plan.dto.meal;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MealUpdateDto {
+
+    private String name;
+    private Integer calories;
+    private Double protein;
+    private Double carbs;
+    private Double fat;
+    private Long nutritionPlanId;
+}
