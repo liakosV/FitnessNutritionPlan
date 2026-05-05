375cbc4d07551cbffbcd9d81efb5b5736fa9c5f0
liakosV <billiak@hotmail.com>
2026-05-05 13:52:11 +0300

    feat: add ExerciseReadDto to handle exercise data retrieval

---
+++ src/main/java/com/project/fitness_nutrition_plan/dto/exercise/ExerciseReadDto.java
@@ -0,0 +1,18 @@
+package com.project.fitness_nutrition_plan.dto.exercise;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExerciseReadDto {
+
+    private Long id;
+    private String name;
+    private Integer sets;
+    private Integer reps;
+    private Integer restTime;
+    private Long workoutDayId;
+}
