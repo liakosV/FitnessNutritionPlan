abc5f18af1fdff34a71048c1ff2b071cb71da686
liakosV <billiak@hotmail.com>
2026-05-05 13:53:36 +0300

    feat: add UserReadDto to handle user data retrieval

---
+++ src/main/java/com/project/fitness_nutrition_plan/dto/user/UserReadDto.java
@@ -0,0 +1,17 @@
+package com.project.fitness_nutrition_plan.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserReadDto {
+
+    private Long id;
+    private String uuid;
+    private String username;
+    private String email;
+    private String role;
+}
