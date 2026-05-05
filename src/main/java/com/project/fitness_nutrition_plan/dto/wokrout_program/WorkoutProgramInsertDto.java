34ca335323e61b47f159dd62ff5ea633eb0b3ecc
liakosV <billiak@hotmail.com>
2026-05-05 13:54:02 +0300

    feat: add WorkoutProgramInsertDto to handle workout program creation requests

---
+++ src/main/java/com/project/fitness_nutrition_plan/dto/wokrout_program/WorkoutProgramInsertDto.java
@@ -0,0 +1,22 @@
+package com.project.fitness_nutrition_plan.dto.wokrout_program;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class WorkoutProgramInsertDto {
+
+    @NotBlank(message = "Name cannot be empty")
+    private String name;
+
+    @NotBlank(message = "Description cannot be empty")
+    private String description;
+
+    private List<Long> assignedUserIds;
+}
