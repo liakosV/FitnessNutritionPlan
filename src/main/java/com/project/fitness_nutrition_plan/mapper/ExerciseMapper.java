1c7020cb652abcc4cd494a64cde9324db868307f
liakosV <billiak@hotmail.com>
2026-05-08 21:32:01 +0300

    feat: add ExerciseMapper to handle mapping between Exercise entity and its DTOs on main

---
+++ src/main/java/com/project/fitness_nutrition_plan/mapper/ExerciseMapper.java
@@ -0,0 +1,24 @@
+package com.project.fitness_nutrition_plan.mapper;
+
+import com.project.fitness_nutrition_plan.dto.exercise.ExerciseInsertDto;
+import com.project.fitness_nutrition_plan.dto.exercise.ExerciseReadDto;
+import com.project.fitness_nutrition_plan.dto.exercise.ExerciseUpdateDto;
+import com.project.fitness_nutrition_plan.model.Exercise;
+import com.project.fitness_nutrition_plan.model.WorkoutDay;
+import org.mapstruct.*;
+
+@Mapper(componentModel = "spring")
+public interface ExerciseMapper {
+
+    @Mapping(target = "workoutDayId", source = "workoutDay.id")
+    ExerciseReadDto mapToExerciseReadDto(Exercise exercise);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "workoutDay", source = "workoutDay")
+    Exercise mapToExercise(ExerciseInsertDto dto, WorkoutDay workoutDay);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "workoutDay", ignore = true)
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateExerciseFromDto(ExerciseUpdateDto dto, @MappingTarget Exercise exercise);
+}
