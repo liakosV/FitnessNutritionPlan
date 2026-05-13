a37632aa540d093bd186c18769372083d3fc639f
liakosV <billiak@hotmail.com>
2026-05-13 21:12:02 +0300

    feat: add new MealRepository methods to support UUID-based lookups and existence checks

--- src/main/java/com/project/fitness_nutrition_plan/repository/MealRepository.java
+++ src/main/java/com/project/fitness_nutrition_plan/repository/MealRepository.java
@@ -4,8 +4,13 @@ import com.project.fitness_nutrition_plan.model.Meal;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MealRepository extends JpaRepository<Meal, Long> {
 
-    List<Meal> findByNutritionPlanId(Long nutritionPlanId);
+    Optional<Meal> findByUuid(String uuid);
+
+    List<Meal> findByNutritionPlanUuid(String nutritionPlanUuid);
+
+    boolean existByUuid(String uuid);
 }
