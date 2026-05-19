fad94105299f0eba02cf7da183d4534b6bb3f91c
liakosV <billiak@hotmail.com>
2026-05-19 20:53:54 +0300

    feat: add UUID-based query methods to ProgressEntryRepository

--- src/main/java/com/project/fitness_nutrition_plan/repository/ProgressEntryRepository.java
+++ src/main/java/com/project/fitness_nutrition_plan/repository/ProgressEntryRepository.java
@@ -1,11 +1,15 @@
 package com.project.fitness_nutrition_plan.repository;
 
 import com.project.fitness_nutrition_plan.model.ProgressEntry;
+import com.project.fitness_nutrition_plan.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProgressEntryRepository extends JpaRepository<ProgressEntry, Long> {
 
-    List<ProgressEntry> findByUserIdOrderByDateDesc(Long userId);
+    List<ProgressEntry> findByUserOrderByDateDesc(User user);
+
+    Optional<ProgressEntry> findByUuid(String uuid);
 }
