fa32c18cdbde517cb5285e0312e53f5e75ee35db
liakosV <billiak@hotmail.com>
2026-05-26 15:17:17 +0300

    feat: add user registration endpoint to AuthRestController

--- src/main/java/com/project/fitness_nutrition_plan/api/AuthRestController.java
+++ src/main/java/com/project/fitness_nutrition_plan/api/AuthRestController.java
@@ -3,12 +3,19 @@ package com.project.fitness_nutrition_plan.api;
 import com.project.fitness_nutrition_plan.authentication.AuthenticationService;
 import com.project.fitness_nutrition_plan.dto.authentication.AuthenticationRequestDto;
 import com.project.fitness_nutrition_plan.dto.authentication.AuthenticationResponseDto;
+import com.project.fitness_nutrition_plan.dto.user.UserInsertDto;
+import com.project.fitness_nutrition_plan.dto.user.UserReadDto;
+import com.project.fitness_nutrition_plan.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -16,10 +23,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthRestController {
 
     private final AuthenticationService authenticationService;
+    private final UserService userService;
 
-    @PostMapping
+    @PostMapping("/login")
     public ResponseEntity<AuthenticationResponseDto> authenticate(@RequestBody AuthenticationRequestDto dto) {
         AuthenticationResponseDto responseDto = authenticationService.authenticate(dto);
         return ResponseEntity.ok(responseDto);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserReadDto> registerUser(@Valid @RequestBody UserInsertDto insertDto) {
+        UserReadDto userReadDto = userService.saveUser(insertDto);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("api/user/{uuid}")
+                .buildAndExpand(userReadDto.getUuid())
+                .toUri();
+
+        return ResponseEntity
+                .created(location)
+                .body(userReadDto);
     }
 }
