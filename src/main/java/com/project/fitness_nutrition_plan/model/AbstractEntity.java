cf341b7630e7f9da9c44f8f2d07b1925c3eeecde
liakosV <billiak@hotmail.com>
2026-05-30 21:02:37 +0300

    feat: enable auditing support by adding AuditingEntityListener to AbstractEntity

--- src/main/java/com/project/fitness_nutrition_plan/model/AbstractEntity.java
+++ src/main/java/com/project/fitness_nutrition_plan/model/AbstractEntity.java
@@ -1,6 +1,7 @@
 package com.project.fitness_nutrition_plan.model;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.PrePersist;
 import lombok.AllArgsConstructor;
@@ -9,11 +10,13 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
 @NoArgsConstructor
