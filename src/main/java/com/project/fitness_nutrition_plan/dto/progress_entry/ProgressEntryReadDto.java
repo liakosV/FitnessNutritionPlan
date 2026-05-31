3d5cb6cc5ed0faa2c9d523963295d3924f1fe964
liakosV <billiak@hotmail.com>
2026-05-31 21:21:00 +0300

    refactor: comment out unused 'id' field in ProgressEntryReadDto

--- src/main/java/com/project/fitness_nutrition_plan/dto/progress_entry/ProgressEntryReadDto.java
+++ src/main/java/com/project/fitness_nutrition_plan/dto/progress_entry/ProgressEntryReadDto.java
@@ -11,7 +11,7 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class ProgressEntryReadDto {
 
-    private Long id;
+//    private Long id;
     private String uuid;
     private Double weight;
     private Double bodyFat;
