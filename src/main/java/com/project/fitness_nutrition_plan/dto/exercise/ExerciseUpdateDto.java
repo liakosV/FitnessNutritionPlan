cb88d4e998c0190081e49844debe959226e10753
liakosV <billiak@hotmail.com>
2026-05-31 21:19:56 +0300

    feat: add validation constraints to ExerciseUpdateDto for sets, reps, and restTime

--- src/main/java/com/project/fitness_nutrition_plan/dto/exercise/ExerciseUpdateDto.java
+++ src/main/java/com/project/fitness_nutrition_plan/dto/exercise/ExerciseUpdateDto.java
@@ -1,5 +1,6 @@
 package com.project.fitness_nutrition_plan.dto.exercise;
 
+import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +11,16 @@ import lombok.NoArgsConstructor;
 public class ExerciseUpdateDto {
 
     private String name;
+
+    @Min(value = 1, message = "Sets must be at least 1")
     private Integer sets;
+
+    @Min(value = 1, message = "Reps must be at least 1")
     private Integer reps;
+
+    @Min(value = 1, message = "Rest time must be at least 1")
     private Integer restTime;
+
+
     private Long workoutDayId;
 }
