87e747bc9a65258b53aec618174b6ba50fccd06e
liakosV <billiak@hotmail.com>
2026-05-31 21:20:23 +0300

    refactor: comment out unused 'id' field in MessageReadDto

--- src/main/java/com/project/fitness_nutrition_plan/dto/message/MessageReadDto.java
+++ src/main/java/com/project/fitness_nutrition_plan/dto/message/MessageReadDto.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class MessageReadDto {
 
-    private Long id;
+//    private Long id;
     private String uuid;
     private String content;
     private LocalDateTime timestamp;
