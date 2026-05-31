50f84d0d35c1d05c34ad5b68c0efc9eff448a32b
liakosV <billiak@hotmail.com>
2026-05-31 21:21:37 +0300

    refactor: update WorkoutDayRepository to use WorkoutProgram object in findByWorkoutProgram method

--- src/main/java/com/project/fitness_nutrition_plan/repository/WorkoutDayRepository.java
+++ src/main/java/com/project/fitness_nutrition_plan/repository/WorkoutDayRepository.java
@@ -1,6 +1,7 @@
 package com.project.fitness_nutrition_plan.repository;
 
 import com.project.fitness_nutrition_plan.model.WorkoutDay;
+import com.project.fitness_nutrition_plan.model.WorkoutProgram;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,5 +11,5 @@ public interface WorkoutDayRepository extends JpaRepository<WorkoutDay, Long> {
 
     Optional<WorkoutDay> findByUuid(String uuid);
 
-    List<WorkoutDay> findByWorkoutProgramUuid(String workoutProgramUuid);
+    List<WorkoutDay> findByWorkoutProgram(WorkoutProgram workoutProgram);
 }
