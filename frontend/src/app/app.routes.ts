d2f498d20b2278a293db0165e832b01391281d8b
liakosV <billiak@hotmail.com>
2026-06-04 11:23:48 +0300

    feat: update routing configuration to set default redirect to login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { Login } from './components/login/login';
 
 export const routes: Routes = [
-    {path: 'login', component: Login}
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: Login },
 ];
