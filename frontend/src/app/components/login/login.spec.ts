70f11e7042fe3d91b883c8346f5d541ccfe400f8
liakosV <billiak@hotmail.com>
2026-06-04 11:24:27 +0300

    test: add Auth provider mock to Login component tests

--- frontend/src/app/components/login/login.spec.ts
+++ frontend/src/app/components/login/login.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
 import { Login } from './login';
+import { Auth } from '../../services/auth';
 
 describe('Login', () => {
   let component: Login;
@@ -8,7 +10,15 @@ describe('Login', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Login]
+      imports: [Login],
+      providers: [
+        {
+          provide: Auth,
+          useValue: {
+            login: () => of({ accessToken: 'access-token', refreshToken: 'refresh-token' }),
+          },
+        },
+      ],
     })
     .compileComponents();
 
