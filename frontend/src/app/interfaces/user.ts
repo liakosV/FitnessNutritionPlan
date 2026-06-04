52679a877791c2d0407161d90f8b4e75cdc8b0d5
liakosV <billiak@hotmail.com>
2026-06-04 11:24:55 +0300

    feat: add authentication interfaces and implement login functionality with token storage

--- frontend/src/app/interfaces/user.ts
+++ frontend/src/app/interfaces/user.ts
@@ -2,5 +2,14 @@ export interface User {
     username: string,
     email: string,
     password: string,
-    
+}
+
+export interface Credentials {
+    username: string,
+    password: string
+}
+
+export interface AuthenticationResponseDto {
+    accessToken: string,
+    refreshToken: string
 }
