f4edc4320d065b18ed047e3b9e863d4eb939c09e
liakosV <billiak@hotmail.com>
2026-06-11 10:55:56 +0300

    feat: add method to retrieve users excluding a specific UUID, ordered by username

--- backend/src/main/java/com/project/fitness_nutrition_plan/repository/UserRepository.java
+++ backend/src/main/java/com/project/fitness_nutrition_plan/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.project.fitness_nutrition_plan.repository;
 import com.project.fitness_nutrition_plan.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -10,6 +11,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
     Optional<User> findByUuid(String uuid);
+    List<User> findByUuidNotOrderByUsernameAsc(String uuid);
 
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
